05dcb2dd198042eee1c0ce975ca43b27a288e2c4
Emerson Alexandre de oliveira Correa <110613926+emersonocorrea@users.noreply.github.com>
2023-12-08 11:50:27 -0300

    finish

---
+++ src/scripts/tailwind.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          bgBody: '#EFEFEF',
+          bgCard1: '#E38525',
+          bgCard2: '#016871',
+          bgCard3: '#003F3E',
+          bgButton: '#F7EFF4'
+        }
+      }
+    }
+  }
